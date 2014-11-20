2b6ed4751e81ce571177b60d25b8d764923a45c5
Matheus Motta <matheusmotta.mm@gmail.com>
2014-11-20 14:08:34 -0200

    Analytics change

--- public/app.js
+++ public/app.js
@@ -33,11 +33,6 @@ angular.module('hackpuc').controller('MainCtrl', function ($http) {
     }
   }
 
-  $('#email-button').on('click', function() {
-    console.log('acadca');
-    ga('send', 'event', 'Share', 'Email');
-  });
-
   if (getMobileOperatingSystem() != 'unknown') {
       $('#whats-app-share').show();
   }
