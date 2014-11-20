223dd91ece0cba9ce5dff6fd2b962c9e3e067278
Matheus Motta <matheusmotta.mm@gmail.com>
2014-11-20 11:16:29 -0200

    Whatsapp visibility bug

--- public/app.js
+++ public/app.js
@@ -18,18 +18,11 @@ angular.module('hackpuc').controller('MainCtrl', function ($http) {
 
   var Environment = {};
 
-  Environment.isMac = navigator.platform.toLowerCase().indexOf('mac') > -1;
-  Environment.isChrome = navigator.userAgent.indexOf('Chrome') > -1;
-  Environment.isSafari = navigator.userAgent.indexOf('Safari') > -1 
-                         && navigator.userAgent.indexOf('Chrome') == -1;
-  Environment.isIE = $.browser.msie;
-  Environment.isFirefox = $.browser.mozilla;
-
   Environment.isiOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false);
   Environment.isAndroid = (navigator.userAgent.indexOf('android') > -1);
 
   var bodyClass;
-  if (!(Environment.isiOS || Environment.isAndroid))
+  if (Environment.isiOS || Environment.isAndroid)
   {
       $('#whats-app-share').show();
   }
